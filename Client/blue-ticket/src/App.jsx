9148a4c6a49cd18947d32c2db9e0f2bc38689e44
Lourdes Jiménez <00090220@uca.edu.sv>
2023-05-19 22:59:23 -0600

    :tada: Begin UI

---
+++ Client/blue-ticket/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className=" p-14 text-lime-500 bg-blue text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
