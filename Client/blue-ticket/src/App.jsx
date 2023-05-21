517ae779e96c9b9cd7dc679806e4386c8dc4f9c6
Roberto640Alvarado <00176620@uca.edu.sv>
2023-05-21 10:25:55 -0600

    :lipstick: Adding Footer and Card components

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbars/NavbarHomepage';
+import Footer from './components/Footer/Footer';
+import CardHome from './components/Card/CardHome';
 
 function App() {
 
   return (
     <>
-      <Navbar/>
+      <CardHome/>
     </>
   )
 }
