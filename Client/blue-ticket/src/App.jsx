26e913c3a599df6f57a1bbca346994b77ca67ba9
omar554 <00023620@uca.edu.sv>
2023-05-26 18:50:31 -0600

    🚧 Add Login, My Ticket, PaymentInfo, ViewQR

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css';
 import Home from './pages/user/Home';
 import FinishedEvents from './pages/admin/FinishedEvents';
 import NewLocation from './pages/admin/NewLocation';
+import MyTickets from './pages/user/MyTIckets.jsx';
+import Login from './pages/user/Login';
+import ViewQR from './pages/user/ViewQR';
+import PaymentInfo from './pages/user/PaymentInfo';
 
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <PaymentInfo/>
     </>
   )
 }
