55735d6a30d4d2c7d7bc9f4e0b64a192d7bc397a
Lourdes Jiménez <00090220@uca.edu.sv>
2023-05-27 21:45:42 -0600

    :lipstick: Add transacction and ticketPurchase pages

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -2,13 +2,15 @@ import './App.css';
 import Home from './pages/user/Home';
 import FinishedEvents from './pages/admin/FinishedEvents';
 import NewLocation from './pages/admin/NewLocation';
+import Transaction from './pages/user/Transaction';
+import TicketPurchase from './pages/user/TicketPurchase';
 
 
 function App() {
 
   return (
     <>
-      <Home/>
+      <TicketPurchase/>
     </>
   )
 }
