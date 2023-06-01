733c8aaffc7831af7661881b4b40b34a34a78713
EA03 <00175420@uca.edu.sv>
2023-05-31 22:08:59 -0600

    :lipstick:Add:User Management View

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -1,21 +1,11 @@
 import './App.css';
 import Home from './pages/user/Home';
-import FinishedEvents from './pages/admin/FinishedEvents';
-import NewLocation from './pages/admin/NewLocation';
-import Transaction from './pages/user/Transaction';
-import TicketPurchase from './pages/user/TicketPurchase';
-import ChangePassword from './pages/user/ChangePassword';
-import TransferTicket from './pages/user/TransferTicket';
-import Record from './pages/user/Record';
-import CreateEvent from './pages/admin/CreateEvent';
-import Pay from './pages/user/Pay';
-
 
 function App() {
 
   return (
     <>
-      <Pay/>
+      <Home/>
     </>
   )
 }
