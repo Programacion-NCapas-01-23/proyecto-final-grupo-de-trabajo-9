031657327d911c2e9a602dd553852f555093e1ee
Roberto640Alvarado <00176620@uca.edu.sv>
2023-06-03 18:36:38 -0600

    :construction: Router Home,Login and Myticket

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css';
-import Home from './pages/user/Home';
-
+import { AppRouter } from './routers/AppRouter';
 function App() {
 
   return (
     <>
-      <PaymentInfo/>
+      <AppRouter/>
     </>
   )
 }
