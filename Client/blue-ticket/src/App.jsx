f45b57624b4f2b3ca14379976046663e470ee29e
omar554 <00023620@uca.edu.sv>
2023-06-04 16:00:01 -0600

    ✅Add: GraphEvent

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css';
+import GraphEvent from './pages/admin/GraphEvent';
 import Home from './pages/user/Home';
 
 function App() {
 
   return (
     <>
-      <PaymentInfo/>
+      <GraphEvent/>
     </>
   )
 }
