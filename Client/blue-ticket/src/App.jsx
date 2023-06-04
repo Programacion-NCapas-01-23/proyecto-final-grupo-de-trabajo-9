b226b2fe556d75d6266d53a1613b42079c1a3ad2
Lourdes Jiménez <00090220@uca.edu.sv>
2023-06-03 19:28:48 -0600

    :lipstick: Add Upcoming events view and list of locations view

--- Client/blue-ticket/src/App.jsx
+++ Client/blue-ticket/src/App.jsx
@@ -4,13 +4,15 @@ import FinishedEvents from './pages/admin/FinishedEvents';
 import NewLocation from './pages/admin/NewLocation';
 import Transaction from './pages/user/Transaction';
 import TicketPurchase from './pages/user/TicketPurchase';
+import UpcomingEvents from './pages/admin/UpcomingEvents';
+import ListOfLocations from './pages/admin/ListOfLocations';
 
 
 function App() {
 
   return (
     <>
-      <TicketPurchase/>
+      <UpcomingEvents/>
     </>
   )
 }
