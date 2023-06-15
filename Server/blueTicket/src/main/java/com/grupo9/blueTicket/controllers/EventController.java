a178ccd6fc0b7e99c57b3e8c1c058858e5d049d9
omar554 <62614708+omar554@users.noreply.github.com>
2023-06-15 16:50:26 -0600

    🚧Add: find one event specific

---
+++ Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/EventController.java
@@ -0,0 +1,29 @@
+package com.grupo9.blueTicket.controllers;
+
+import com.grupo9.blueTicket.models.dtos.EventDTO;
+import com.grupo9.blueTicket.services.EventService;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/events")
+public class EventController {
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDTO> getEventById(@PathVariable UUID eventId) {
+        EventDTO eventDTO = eventService.getEventById(eventId);
+        return ResponseEntity.status(HttpStatus.OK).body(eventDTO);
+    }
+}
