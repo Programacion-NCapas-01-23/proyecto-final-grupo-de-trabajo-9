ccdc15b9e6a3eac8ee65932336af55bf119f0ec7
EA03 <00175420@uca.edu.sv>
2023-06-17 20:41:10 -0600

    :white_check_mark:Add Event Controller

---
+++ Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/EventController1.java
@@ -0,0 +1,64 @@
+package com.grupo9.blueTicket.controllers;
+
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.grupo9.blueTicket.models.dtos.MessageDTO;
+import com.grupo9.blueTicket.models.dtos.SaveEventDTO;
+import com.grupo9.blueTicket.models.entities.Event;
+import com.grupo9.blueTicket.services.EventService;
+import com.grupo9.blueTicket.utils.RequestErrorHandler;
+
+import jakarta.validation.Valid;
+import net.bytebuddy.build.Plugin.Engine.ErrorHandler;
+
+@RestController
+@RequestMapping("/events")
+public class EventController1 {
+	
+	private EventService eventService;
+	
+	private RequestErrorHandler errorHandler;
+	
+	@PostMapping("/save")
+	public ResponseEntity<?> saveEvent(@RequestBody @Valid SaveEventDTO info, BindingResult validations){
+		if(validations.hasErrors()) {
+			return new ResponseEntity<>(
+					errorHandler.mapErrors(validations.getFieldErrors()), 
+					HttpStatus.BAD_REQUEST);
+		}
+		
+		try {
+			eventService.createEvent(info);
+			return new ResponseEntity<>(
+					new MessageDTO("Event create" + info), HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(
+					new MessageDTO("Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+	}
+	
+	@GetMapping("/events/{id}")
+	public ResponseEntity<?> getEventById(@PathVariable UUID id){
+		Event event = eventService.findOneById(id);
+		if(event != null) {
+			return ResponseEntity.ok(event);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	
+	
+
+}
