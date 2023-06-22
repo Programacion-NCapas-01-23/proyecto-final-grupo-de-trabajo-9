be769db4c8cc4dbdef068286b0b1a12bb162b2bf
Lourdes Jiménez <00090220@uca.edu.sv>
2023-06-22 17:27:57 -0600

    :construction: Add create new ticket route

--- Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/EventController1.java
+++ Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/EventController1.java
@@ -1,7 +1,5 @@
 package com.grupo9.blueTicket.controllers;
 
-import java.sql.Date;
-import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,22 +11,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.grupo9.blueTicket.models.dtos.ActiveDTO;
 import com.grupo9.blueTicket.models.dtos.ActiveEventDTO;
 import com.grupo9.blueTicket.models.dtos.MessageDTO;
 import com.grupo9.blueTicket.models.dtos.SaveEventDTO;
-import com.grupo9.blueTicket.models.entities.Category;
 import com.grupo9.blueTicket.models.entities.Event;
-import com.grupo9.blueTicket.services.CategoryService;
 import com.grupo9.blueTicket.services.EventService;
 import com.grupo9.blueTicket.utils.RequestErrorHandler;
 
 import jakarta.validation.Valid;
-import net.bytebuddy.build.Plugin.Engine.ErrorHandler;
 
 @RestController
 @RequestMapping("/events")
@@ -54,6 +49,30 @@ public class EventController1 {
 			eventService.createEvent(info);
 			return new ResponseEntity<>(
 					new MessageDTO("Event created " +info), HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(
+					new MessageDTO("Internal Server Error"), HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+	}
+	@PutMapping("/update/{id}")
+	public ResponseEntity<?> updateEvent(@PathVariable(name = "id") UUID id,@RequestBody @Valid SaveEventDTO info, BindingResult validations){
+		if(validations.hasErrors()) {
+			return new ResponseEntity<>(
+					errorHandler.mapErrors(validations.getFieldErrors()), 
+					HttpStatus.BAD_REQUEST);
+		}
+		Event event = eventService.findOneById(id);
+		
+		if (event == null) {
+			return new ResponseEntity<>(
+		    		new MessageDTO("This event was not found"),
+		    		HttpStatus.NOT_FOUND);
+		}
+		try {
+			eventService.updateCreatedEvent(id,info);
+			return new ResponseEntity<>(
+					new MessageDTO("This event was updated " +info), HttpStatus.CREATED);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(
@@ -92,8 +111,8 @@ public class EventController1 {
 		return new ResponseEntity<>(allEvents, HttpStatus.OK);
 	}
 	
-	@GetMapping("/events/{id}")
-	public ResponseEntity<?> getEventById(@PathVariable UUID id){
+	@GetMapping("/event/{id}")
+	public ResponseEntity<?> getEventById(@PathVariable(name = "id") UUID id){
 		Event event = eventService.findOneById(id);
 		if(event != null) {
 			return ResponseEntity.ok(event);
