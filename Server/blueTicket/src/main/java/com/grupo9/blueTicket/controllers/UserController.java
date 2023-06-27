edd1d945b47786f5438e96be42f41d3b56adfb30
Lourdes Jiménez <00090220@uca.edu.sv>
2023-06-27 10:11:59 -0600

    :adhesive_bandage: Solve possible cross problem

--- Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/UserController.java
+++ Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin("*")
 public class UserController {
 	
 	@Autowired
