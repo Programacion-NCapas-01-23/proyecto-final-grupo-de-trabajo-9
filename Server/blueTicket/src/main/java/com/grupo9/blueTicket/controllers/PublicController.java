53fb3c416cbb84d1a85c18f68847945e4c5ff6cc
Lourdes Jiménez <00090220@uca.edu.sv>
2023-07-04 00:07:59 -0600

    :sparkles: Add new route

--- Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/PublicController.java
+++ Server/blueTicket/src/main/java/com/grupo9/blueTicket/controllers/PublicController.java
@@ -1,6 +1,7 @@
 package com.grupo9.blueTicket.controllers;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,12 +10,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.grupo9.blueTicket.models.entities.Event;
+import com.grupo9.blueTicket.models.entities.User;
 import com.grupo9.blueTicket.services.EventService;
+import com.grupo9.blueTicket.services.UserService;
+import com.grupo9.blueTicket.models.dtos.EmailDTO;
+import com.grupo9.blueTicket.models.dtos.MessageDTO;
 import com.grupo9.blueTicket.models.dtos.PageDTO;
 
 @RestController
@@ -24,6 +30,8 @@ public class PublicController {
 	
 	@Autowired
 	private EventService eventService;
+	@Autowired
+	private UserService userService;
 	
 	
 	@GetMapping("/home")
@@ -51,4 +59,21 @@ public class PublicController {
 
 	    return new ResponseEntity<>(songPageDTO, HttpStatus.OK);
 	}
+	
+	@GetMapping("/get-user")
+	public ResponseEntity<?> getUserId(@RequestBody EmailDTO email){
+		List<User> user = userService.findAll();
+		UUID id = null;
+		for(User u : user) {
+			if (u.getEmail().equals(email.getEmail())) {
+				id = u.getId();
+			}
+		}
+		if(id != null) {
+			return new ResponseEntity<>(id, HttpStatus.OK);
+		}else {
+			return ResponseEntity.notFound().build();
+		}
+	}
 }
+
---
+++ Server/blueTicket/src/main/java/com/grupo9/blueTicket/models/dtos/EmailDTO.java
@@ -0,0 +1,9 @@
+package com.grupo9.blueTicket.models.dtos;
+
+import lombok.Data;
+
+@Data
+public class EmailDTO {
+	
+	private String email;
+}
