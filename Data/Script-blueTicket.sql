ae73ed78524c429cf0a7c8ae063215b966b33314
Lourdes Jiménez <00090220@uca.edu.sv>
2023-07-04 23:14:19 -0600

    :memo: Add database final Script

--- Data/Script-blueTicket.sql
+++ Data/Script-blueTicket.sql
@@ -1,19 +1,3 @@
-
-CREATE TABLE public."token" (
-code uuid NOT NULL DEFAULT gen_random_uuid(),
-"content" varchar NOT NULL,
-active boolean NOT NULL DEFAULT true,
-"timestamp" timestamp without time zone NULL DEFAULT CURRENT_TIMESTAMP,
-user_code uuid NULL,
-CONSTRAINT token_pk PRIMARY KEY (code),
-CONSTRAINT token_fk FOREIGN KEY (user_code) REFERENCES public."user"(id) ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-
-
--- public."access" foreign keys
-
---ALTER TABLE public."access" ADD CONSTRAINT access_fk FOREIGN KEY (user_id) REFERENCES public."user"(id) ON DELETE CASCADE ON UPDATE CASCADE;
 -- public.category definition
 
 -- Drop table
@@ -21,11 +5,11 @@ CONSTRAINT token_fk FOREIGN KEY (user_code) REFERENCES public."user"(id) ON DELE
 -- DROP TABLE public.category;
 
 CREATE TABLE public.category (
-	description varchar NOT NULL,
-	status varchar NOT NULL,
+	"name" varchar NOT NULL,
 	id int4 NOT NULL,
 	CONSTRAINT category_pk PRIMARY KEY (id)
 );
+
 -- public."event" definition
 
 -- Drop table
@@ -39,12 +23,11 @@ CREATE TABLE public."event" (
 	"hour" time NOT NULL,
 	duration varchar NOT NULL,
 	sponsors varchar NOT NULL,
-	involved varchar NOT NULL,
-	id_user uuid NOT NULL,
+	artist varchar NOT NULL,
 	main_image varchar NOT NULL,
 	secondary_image varchar NOT NULL,
-	id_locality uuid NOT NULL,
 	id_category int4 NOT NULL,
+	status bool NOT NULL DEFAULT true,
 	CONSTRAINT event_pk PRIMARY KEY (id)
 );
 
@@ -52,8 +35,6 @@ CREATE TABLE public."event" (
 -- public."event" foreign keys
 
 ALTER TABLE public."event" ADD CONSTRAINT event_fk FOREIGN KEY (id_category) REFERENCES public.category(id);
-ALTER TABLE public."event" ADD CONSTRAINT event_fk_1 FOREIGN KEY (id_locality) REFERENCES public.locality(id);
-ALTER TABLE public."event" ADD CONSTRAINT event_fk_2 FOREIGN KEY (id_user) REFERENCES public."user"(id);
 
 -- public.locality definition
 
@@ -66,8 +47,14 @@ CREATE TABLE public.locality (
 	"name" varchar NOT NULL,
 	price float4 NOT NULL,
 	capacity int4 NOT NULL,
+	id_event uuid NOT NULL,
 	CONSTRAINT locality_pk PRIMARY KEY (id)
 );
+
+
+-- public.locality foreign keys
+
+ALTER TABLE public.locality ADD CONSTRAINT locality_fk FOREIGN KEY (id_event) REFERENCES public."event"(id);
 
 -- public."permission" definition
 
@@ -77,7 +64,6 @@ CREATE TABLE public.locality (
 
 CREATE TABLE public."permission" (
 	"permission" varchar NOT NULL,
-	status bool NOT NULL,
 	id int4 NOT NULL,
 	CONSTRAINT permission_pk PRIMARY KEY (id)
 );
@@ -89,15 +75,9 @@ CREATE TABLE public."permission" (
 
 CREATE TABLE public."role" (
 	"role" varchar NOT NULL,
-	id_role_permission uuid NOT NULL,
 	id int4 NOT NULL,
 	CONSTRAINT role_pk PRIMARY KEY (id)
 );
-
-
--- public."role" foreign keys
-
-ALTER TABLE public."role" ADD CONSTRAINT role_fk FOREIGN KEY (id_role_permission) REFERENCES public.role_permission(id);
 
 -- public.role_permission definition
 
@@ -129,13 +109,16 @@ CREATE TABLE public.sale (
 	id_user uuid NOT NULL,
 	date_purchase timestamp NOT NULL,
 	amount_tickets_purchased int4 NOT NULL,
+	id_ticket uuid NOT NULL,
+	total_amount int4 NOT NULL,
 	CONSTRAINT sale_pk PRIMARY KEY (id)
 );
 
 
 -- public.sale foreign keys
 
-ALTER TABLE public.sale ADD CONSTRAINT sale_fk FOREIGN KEY (id_user) REFERENCES public."user"(id);
+ALTER TABLE public.sale ADD CONSTRAINT sale_fk FOREIGN KEY (id_user) REFERENCES public.client(id);
+ALTER TABLE public.sale ADD CONSTRAINT sale_fk_1 FOREIGN KEY (id_ticket) REFERENCES public.ticket(id);
 
 -- public.ticket definition
 
@@ -145,10 +128,8 @@ ALTER TABLE public.sale ADD CONSTRAINT sale_fk FOREIGN KEY (id_user) REFERENCES 
 
 CREATE TABLE public.ticket (
 	id uuid NOT NULL DEFAULT gen_random_uuid(),
-	description varchar NOT NULL,
 	status bool NOT NULL,
 	id_event uuid NULL,
-	id_sale uuid NULL,
 	CONSTRAINT ticket_pk PRIMARY KEY (id)
 );
 
@@ -156,7 +137,26 @@ CREATE TABLE public.ticket (
 -- public.ticket foreign keys
 
 ALTER TABLE public.ticket ADD CONSTRAINT ticket_fk FOREIGN KEY (id_event) REFERENCES public."event"(id);
-ALTER TABLE public.ticket ADD CONSTRAINT ticket_fk_1 FOREIGN KEY (id_sale) REFERENCES public.sale(id);
+
+-- public."token" definition
+
+-- Drop table
+
+-- DROP TABLE public."token";
+
+CREATE TABLE public."token" (
+	code uuid NOT NULL DEFAULT gen_random_uuid(),
+	"content" varchar NOT NULL,
+	active bool NOT NULL DEFAULT true,
+	"timestamp" timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	user_code uuid NULL,
+	CONSTRAINT token_pk PRIMARY KEY (code)
+);
+
+
+-- public."token" foreign keys
+
+ALTER TABLE public."token" ADD CONSTRAINT token_fk FOREIGN KEY (user_code) REFERENCES public.client(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- public.transfer definition
 
@@ -176,30 +176,87 @@ CREATE TABLE public.transfer (
 
 -- public.transfer foreign keys
 
-ALTER TABLE public.transfer ADD CONSTRAINT transfer_fk FOREIGN KEY (id_user_issuer) REFERENCES public."user"(id);
-ALTER TABLE public.transfer ADD CONSTRAINT transfer_fk_1 FOREIGN KEY (id_user_receptor) REFERENCES public."user"(id);
+ALTER TABLE public.transfer ADD CONSTRAINT transfer_fk FOREIGN KEY (id_user_issuer) REFERENCES public.client(id);
+ALTER TABLE public.transfer ADD CONSTRAINT transfer_fk_1 FOREIGN KEY (id_user_receptor) REFERENCES public.client(id);
 ALTER TABLE public.transfer ADD CONSTRAINT transfer_fk_2 FOREIGN KEY (id_ticket) REFERENCES public.ticket(id);
 
--- public."user" definition
+-- public.client definition
 
 -- Drop table
 
--- DROP TABLE public."user";
+-- DROP TABLE public.client;
 
-CREATE TABLE public."user" (
+CREATE TABLE public.client (
 	id uuid NOT NULL DEFAULT gen_random_uuid(),
 	"name" varchar NOT NULL,
 	email varchar NOT NULL,
 	"password" varchar NOT NULL,
-	active boolean NOT NULL DEFAULT true,
-	id_role int4 NOT NULL DEFAULT 1,
+	active bool NOT NULL DEFAULT true,
 	CONSTRAINT user_pk PRIMARY KEY (id),
 	CONSTRAINT user_un UNIQUE (email)
 );
 
+-- public.user_role definition
+
+-- Drop table
+
+-- DROP TABLE public.user_role;
+
+CREATE TABLE public.user_role (
+	id uuid NOT NULL DEFAULT gen_random_uuid(),
+	id_user uuid NOT NULL,
+	id_role int4 NOT NULL,
+	status bool NOT NULL,
+	CONSTRAINT user_role_pk PRIMARY KEY (id)
+);
 
 
--- public."user" foreign keys
+-- public.user_role foreign keys
 
-ALTER TABLE public."user" ADD CONSTRAINT user_fk FOREIGN KEY (id_role) REFERENCES public."role"(id);
---ALTER TABLE public."user" ADD CONSTRAINT user_fk_1 FOREIGN KEY (id_access) REFERENCES public."access"(id);
+ALTER TABLE public.user_role ADD CONSTRAINT user_role_fk FOREIGN KEY (id_role) REFERENCES public."role"(id);
+ALTER TABLE public.user_role ADD CONSTRAINT user_role_fk_1 FOREIGN KEY (id_user) REFERENCES public.client(id);
+
+-- Add Role
+
+INSERT INTO public."role"
+("role", id)
+VALUES('Cliente', 1);
+
+INSERT INTO public."role"
+("role", id)
+VALUES('Admin', 2);
+
+INSERT INTO public."role"
+("role", id)
+VALUES('Moderador', 3);
+
+INSERT INTO public."role"
+("role", id)
+VALUES('SysAdmin', 4);
+
+INSERT INTO public."role"
+("role", id)
+VALUES('LectorQR', 5);
+
+-- Add Category
+
+INSERT INTO public.category
+("name", id)
+VALUES('Danza', 1);
+
+INSERT INTO public.category
+("name", id)
+VALUES('Deportes', 2);
+
+INSERT INTO public.category
+("name", id)
+VALUES('Conferencia', 3);
+
+INSERT INTO public.category
+("name", id)
+VALUES('Teatro', 4);
+
+INSERT INTO public.category
+("name", id)
+VALUES('Concierto', 5);
+
